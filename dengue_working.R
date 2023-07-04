257e0fceb10f2e625a27646336bee95b0abd8893
Wit <mariken.dewit@wur.nl>
2023-07-04 18:02:25 +0200

    added years, changed to array

--- dengue_working.R
+++ dengue_working.R
@@ -1,21 +1,22 @@
+rm(list=ls())
 # we want to recreate the dengue model
 
 # we first create the four probabilities of infection 
 getwd()
 library(tidyverse)
+library(data.table)
 
 # Set parameters ----------------------------------------------------------
 
 max_age <- 100
+years <- 2010:2030
 N0 <- 100 # population size
 no_infection <- N0
 
 
 # Initialise dataframe ----------------------------------------------------
 
-col_length <- rep(0, max_age+1)
-
-col_names <- list("age",
+col_names <- list(
                "S4",
                "S3",
                "S2",
@@ -27,12 +28,21 @@ col_names <- list("age",
                "I4",
                "total")
 
-dengue <- array(data = 0,
-                dim = c(length(col_length),length(col_names)))
-colnames(dengue) <- col_names
-dengue[,"age"] <- seq( 0, max_age, by = 1)
-dengue[1,"no_infected"] <- 100
+array_names <- vector("list", 3)
+array_names[[1]] <- years
+array_names[[2]] <- (1:(max_age+1) -1)
+array_names[[3]] <- col_names
 
+# dims are year - age - outcomes
+dengue <- array(data = 0,
+                dim = c(length(years), max_age+1,length(col_names)),
+                dimnames=array_names)
+
+
+
+dengue["2010",,"S4"] <- 100 # add 100 to S4 to each age group in 2010
+
+dengue[,"0","S4"] <- 100 # add 100 to S4 of 0 age group in all years
 
 # Populate dataframe ------------------------------------------------------
 
@@ -40,62 +50,73 @@ infection_probs <- function( lambda, max_age, df, verbose=1 ){
   if(verbose==1){browser()}
   
   # infection probabilities
-  infect1_0 <- 1-(1- lambda)^4
-  infect2_1 <- 1-(1- lambda)^3
-  infect3_2 <- 1-(1- lambda)^2
-  infect4_3 <- 1-(1- lambda)^1
-  infect5_4 <- 0L
+  P_I0_I1 <- 1-(1 - lambda)^4
+  P_I1_I2 <- 1-(1 - lambda)^3
+  P_I2_I3 <- 1-(1 - lambda)^2
+  P_I3_I4 <- 1-(1 - lambda)^1
+  P_I4_I5 <- 0L
   
-
-  for(age in seq_len(max_age)) {
-    # S4
-    df[y+1, age + 1 , "S4"] <- df[y, age, "S4"] * (1-infect1_0) # probability of individual staying susceptible
+  for (y in 1:(length(years)-1)) {
     
-    # S3
-    df[y+1, age + 1, "S3"] <- df[y, age, "S4"] * (infect1_0) + # prob of people with first infection 
-      (df[y, age, "S3"] * (1-infect2_1) ) # probability of not getting a second infection
-
-    # I1 (reminder: infections occur during this age and year)
-    df[y, age, "I1"] <- df[y,age, "S4"] * (infect1_0)  # Number of new first infections
+    #last age can't get new infections
+    for(age in seq_len(max_age)) {
+      # S4
+      df[y+1, age + 1 , "S4"] <- df[y, age, "S4"] * (1-P_I0_I1) # probability of individual staying susceptible
+      
+      # S3
+      df[y+1, age + 1, "S3"] <- df[y, age, "S4"] * (P_I0_I1) + # prob of people with first infection 
+        (df[y, age, "S3"] * (1-P_I1_I2) ) # probability of not getting a second infection
+      
+      # I1 (reminder: infections occur during this age and year)
+      df[y, age, "I1"] <- df[y,age, "S4"] * (P_I0_I1)  # Number of new first infections
+      
+      # S2
+      df[y+1, age+1, "S2"] <- df[y, age, "S3"] * (P_I1_I2) + # prob of people with second infection 
+        df[y, age, "S2"] * (1-P_I2_I3) # probability of not getting a third infection
+      
+      # I2
+      df[y, age, "I2"] <- df[y, age, "S3"] * (P_I1_I2)   # Number of new second infections
+      
+      # S1
+      df[y+1, age+1, "S1"] <- df[y, age, "S2"] * (P_I2_I3) + # prob of people with third infection 
+        (df[y, age, "S1"] * (1-P_I3_I4) ) # probability of not getting a fourth infection
+      
+      #I3
+      df[y, age, "I3"] <- df[y, age, "S2"] * (P_I2_I3) # number of new third infections
+      
+      # S0
+      df[y+1, age+1, "S0"] <- df[y, age, "S1"] * (P_I3_I4) + # prob of people with fourth infection 
+        (df[y, age, "S0"] * (1-P_I4_I5) ) # probability of not getting a fifth infection
+      
+      # I4
+      df[y, age, "I4"] <- df[y, age, "S1"] * (P_I3_I4) # number of new fourth infections
+      
+    }
     
-    # S2
-    df[age, "S2"] <- df[age -1, "S3"] * (infect2_1) + # prob of people with second infection 
-      (df[age-1, "S2"] * (1-infect3_2) ) # probability of not getting a third infection
+    # df[,"total"] <- rowSums(df[,, which(col_names %like% "S")])
     
-    # I2
-    df[age, "I2"] <- df[age -1, "S3"] * (infect2_1)   # Number of new second infections
-    
-    # S1
-    df[age, "S1"] <- df[age -1, "S2"] * (infect3_2) + # prob of people with third infection 
-      (df[age-1, "S1"] * (1-infect4_3) ) # probability of not getting a fourth infection
-    
-    #I3
-    df[age, "I3"] <- df[age -1, "S2"] * (infect3_2) # number of new third infections
-    
-    # S0
-    df[age, "S0"] <- df[age -1, "S1"] * (infect4_3) + # prob of people with fourth infection 
-      (df[age-1, "S0"] * (1-infect5_4) ) # probability of not getting a fifth infection
-    
-    # I4
-    df[age, "I4"] <- df[age -1, "S1"] * (infect4_3) # number of new fourth infections
-    
+   
   }
-  
-  df[,"total"] <- rowSums(df[, "S4":"S0"])
-  
   return(df)
 }
-
 
 # Plotting ----------------------------------------------------------------
 
 dengue_lambda_Lo <- infection_probs( lambda = 0.01, max_age = max_age, df = dengue, verbose = 0 )
 
+dengue_lambda_Lo["2030", , ]
+
 dengue_lambda_Hi <- infection_probs( lambda = 0.05, max_age = max_age, df = dengue, verbose = 0 )
 
+dengue_lambda_Lo["2010",,"I2"]
 
-plot( x= dengue_lambda_Lo[,"age"], y = dengue_lambda_Lo[,"new_second"], type = "l", ylim = c(0, 5))
-lines( x = dengue_lambda_Hi[,"age"], y = dengue_lambda_Hi[,"new_second"])
+plot( x=(1:(max_age+1) -1), y = dengue_lambda_Lo["2015",,"I2" ], type = "l", 
+      ylim = c(0, 2), xlab="Ages", ylab="Number of new secondary infections")
+
+for(i in 2:(length(years))){
+  lines( x = (1:(max_age+1) -1), y = dengue_lambda_Lo[i,,"I2" ])
+  
+}
 
  
 
