2e4b02f55196f8dd39a3348c46ebbcd425e6d539
malkuzweny <manaralkuzweny@gmail.com>
2023-07-04 11:09:09 +0200

    changed data structure to array

--- dengue_working.R
+++ dengue_working.R
@@ -1,9 +1,9 @@
-# we want to recreate the Dengue model. 
+# we want to recreate the dengue model
 
-# we frist create the four probabiilties of infection 
+# we first create the four probabilities of infection 
 getwd()
 library(tidyverse)
-library(ggplot)
+library(ggplot2)
 
 #============ infection P
 
@@ -14,23 +14,26 @@ N0 <- 100
 
 no_infection <- N0
 
-col_length <- rep(0, max_age +1)
+col_length <- rep(0, max_age+1)
 
-dengue <- tibble( 
-  age = seq( 0, max_age, by = 1), 
-  no_infected = c(100, rep(0, max_age )),
-  first_inf = col_length,
-  sec_inf = col_length, 
-  third_inf = col_length,
-  fourth_inf = col_length,
-  new_first = col_length, 
-  new_second = col_length, 
-  new_third = col_length, 
-  new_fourth = col_length)
+col_names <- list("age",
+               "no_infected",
+               "first_inf",
+               "sec_inf",
+               "third_inf",
+               "fourth_inf",
+               "new_first",
+               "new_second",
+               "new_third",
+               "new_fourth",
+               "total")
 
+dengue <- array(data = 0,
+                dim = c(length(col_length),length(col_names)))
+colnames(dengue) <- col_names
+dengue[,"age"] <- seq( 0, max_age, by = 1)
+dengue[1,"no_infected"] <- 100
 
-
-infect1_0
 infection_probs <- function( lambda, max_age, df, verbose=1 ){
   if(verbose==1){browser()}
   
@@ -42,7 +45,7 @@ infection_probs <- function( lambda, max_age, df, verbose=1 ){
   infect5_4 <- 0L
   
   
-  # first column(Numbe of people who are susceuptible)
+  # first column(Number of people who are susceptible)
   for( ii in 2:(max_age+1)) {
     
     df[ii, 2] <- df[ii -1, 2] * (1-infect1_0) # probability 
@@ -53,7 +56,7 @@ infection_probs <- function( lambda, max_age, df, verbose=1 ){
   for( ii in 2:(max_age+1)) {
     
     df[ii, 3] <- df[ii -1, 2] * (infect1_0) + # prob of people with first infection 
-      (df[ii-1, 3] * (1-infect2_1) ) # probability of not gettign a second infection
+      (df[ii-1, 3] * (1-infect2_1) ) # probability of not getting a second infection
     
     
     df[ii, 7] <- df[ii -1, 2] * (infect1_0)  # Number of new cases
@@ -65,7 +68,7 @@ infection_probs <- function( lambda, max_age, df, verbose=1 ){
   for( ii in 2:(max_age+1)) {
     
     df[ii, 4] <- df[ii -1, 3] * (infect2_1) + # prob of people with second infection 
-      (df[ii-1, 4] * (1-infect3_2) ) # probability of not gettign a third infection
+      (df[ii-1, 4] * (1-infect3_2) ) # probability of not getting a third infection
     
     df[ii, 8] <- df[ii -1, 3] * (infect2_1)   # Number of new cases
   }
@@ -75,7 +78,7 @@ infection_probs <- function( lambda, max_age, df, verbose=1 ){
   for( ii in 2:(max_age+1)) {
     
     df[ii, 5] <- df[ii -1, 4] * (infect3_2) + # prob of people with third infection 
-      (df[ii-1, 5] * (1-infect4_3) ) # probability of not gettign a fourth infection
+      (df[ii-1, 5] * (1-infect4_3) ) # probability of not getting a fourth infection
     
     df[ii, 9] <- df[ii -1, 4] * (infect3_2) # number of new cases 
   }
@@ -85,12 +88,12 @@ infection_probs <- function( lambda, max_age, df, verbose=1 ){
   for( ii in 2:(max_age+1)) {
     
     df[ii, 6] <- df[ii -1, 5] * (infect4_3) + # prob of people with third infection 
-      (df[ii-1, 6] * (1-infect5_4) ) # probability of not gettign a fourth infection
+      (df[ii-1, 6] * (1-infect5_4) ) # probability of not getting a fourth infection
     
     df[ii, 10] <- df[ii -1, 5] * (infect4_3) # number of new cases 
   }
   
-  df<- df %>% mutate( total = rowSums(df[, 2:6])) 
+  df[,"total"] <- rowSums(df[, 2:6])
   
   return(df)
 }
@@ -98,22 +101,11 @@ infection_probs <- function( lambda, max_age, df, verbose=1 ){
 
 dengue_lambda_Lo <- infection_probs( lambda = 0.01, max_age = max_age, df = dengue, verbose = 0 )
 
-
 dengue_lambda_Hi <- infection_probs( lambda = 0.05, max_age = max_age, df = dengue, verbose = 0 )
 
-sum(dengue_lambda_Lo$new_first)
-sum(dengue_lambda_Lo$new_second)
-sum(dengue_lambda_Lo$new_third)
-sum(dengue_lambda_Lo$new_fourth)
 
-infect1_0 <- (1-(1- lambda)^400) * (1-(1- lambda)^400)
-
-dengue_lambda_Lo
-
-dengue_lambda_Lo%>%view()
-
- plot( x= dengue_lambda_Lo$age, y = dengue_lambda_Lo$new_second, type = "l", ylim = c(0, 5))
- lines( x = dengue_lambda_Hi$age, y = dengue_lambda_Hi$new_second)
+plot( x= dengue_lambda_Lo[,"age"], y = dengue_lambda_Lo[,"new_second"], type = "l", ylim = c(0, 5))
+lines( x = dengue_lambda_Hi[,"age"], y = dengue_lambda_Hi[,"new_second"])
 
  
 
