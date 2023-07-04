8271b23ec2afab509b18f73298d308e031e9e435
malkuzweny <manaralkuzweny@gmail.com>
2023-07-04 15:13:38 +0200

    update after meeting w bobby

--- dengue_working.R
+++ dengue_working.R
@@ -7,7 +7,6 @@ library(ggplot2)
 
 #============ infection P
 
-lambda <- 0.01
 max_age <- 100
 
 N0 <- 100 
@@ -44,31 +43,34 @@ infection_probs <- function( lambda, max_age, df, verbose=1 ){
   infect4_3 <- 1-(1- lambda)^1
   infect5_4 <- 0L
   
-  
+
   # first column(Number of people who are susceptible)
-  for( ii in 2:(max_age+1)) {
+  for(age in seq_len(max_age)) {
+    # S4
+    df[y+1, age + 1 , 2] <- df[y, age, 2] * (1-infect1_0) # probability of individual staying susceptible
     
-    df[ii, 2] <- df[ii -1, 2] * (1-infect1_0) # probability of individual staying susceptible
+    # S3
+    df[y+1, age + 1, "S3"] <- df[y, age, "S4"] * (infect1_0) + # prob of people with first infection 
+      (df[y, age, "S3"] * (1-infect2_1) ) # probability of not getting a second infection
     
-    df[ii, 3] <- df[ii -1, 2] * (infect1_0) + # prob of people with first infection 
-      (df[ii-1, 3] * (1-infect2_1) ) # probability of not getting a second infection
+    #df[age-1,7]
+    # infections occur during this age and year
+    df[y, age, "I1"] <- df[y,age, "S4"] * (infect1_0)  # Number of new first infections
     
-    df[ii, 7] <- df[ii -1, 2] * (infect1_0)  # Number of new first infections
+    df[age, 4] <- df[age -1, 3] * (infect2_1) + # prob of people with second infection 
+      (df[age-1, 4] * (1-infect3_2) ) # probability of not getting a third infection
     
-    df[ii, 4] <- df[ii -1, 3] * (infect2_1) + # prob of people with second infection 
-      (df[ii-1, 4] * (1-infect3_2) ) # probability of not getting a third infection
+    df[age, 8] <- df[age -1, 3] * (infect2_1)   # Number of new second infections
     
-    df[ii, 8] <- df[ii -1, 3] * (infect2_1)   # Number of new second infections
+    df[age, 5] <- df[age -1, 4] * (infect3_2) + # prob of people with third infection 
+      (df[age-1, 5] * (1-infect4_3) ) # probability of not getting a fourth infection
     
-    df[ii, 5] <- df[ii -1, 4] * (infect3_2) + # prob of people with third infection 
-      (df[ii-1, 5] * (1-infect4_3) ) # probability of not getting a fourth infection
+    df[age, 9] <- df[age -1, 4] * (infect3_2) # number of new third infections
     
-    df[ii, 9] <- df[ii -1, 4] * (infect3_2) # number of new third infections
+    df[age, 6] <- df[age -1, 5] * (infect4_3) + # prob of people with fourth infection 
+      (df[age-1, 6] * (1-infect5_4) ) # probability of not getting a fifth infection
     
-    df[ii, 6] <- df[ii -1, 5] * (infect4_3) + # prob of people with fourth infection 
-      (df[ii-1, 6] * (1-infect5_4) ) # probability of not getting a fifth infection
-    
-    df[ii, 10] <- df[ii -1, 5] * (infect4_3) # number of new fourth infections
+    df[age, 10] <- df[age -1, 5] * (infect4_3) # number of new fourth infections
     
   }
   
