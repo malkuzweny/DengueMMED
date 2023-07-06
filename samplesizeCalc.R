a12d5cf9e7572f9a7f3fb59dff08dd31b6254223
Wit <mariken.dewit@wur.nl>
2023-07-06 14:48:25 +0200

    added file for sample size calculation

---
+++ samplesizeCalc.R
@@ -0,0 +1,19 @@
+
+# Do power calculation ----------------------------------------------------
+
+run.sscalc <- function(z_a2, z_b, pi_0, treatment_effect, k, nr.percluster) {
+
+  # pi_0 = proportion of population experiencing second infection during trial at baseline
+  # pi_a = proportion of population experiencing second infection during trial in intervention group
+  
+  pi_a <- pi_0 * (1-treatment_effect)
+  
+  clusters_perarm <- 2 + 
+                    (z_a2 + z_b)^2 * 
+                    ((pi_0 * (1-pi_0)/nr.percluster) + (pi_a * (1-pi_a)/nr.percluster) + k^2*(pi_0^2 + pi_a^2)) / (pi_0 - pi_a)^2 
+  
+  return(clusters_perarm)
+}
+
+test <- run.sscalc(z_a2=1.96, z_b=0.84, pi_0=0.1, treatment_effect = 0.3, k=0.15, nr.percluster = 100)
+
