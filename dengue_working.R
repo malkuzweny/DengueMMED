792425ef4fc8bc592e9367f1738970a9bcb02362
malkuzweny <manaralkuzweny@gmail.com>
2023-09-06 11:01:39 -0400

    changed from 4 ST to 3 ST model

--- dengue_working.R
+++ dengue_working.R
@@ -9,8 +9,9 @@ library(data.table)
 # Calculate lambda in Colombo
 
 # probability first infection = 14.1%
-# 0.141 <- 1-(1 - lambda)^4
-# lambda = 0.037
+# 0.141 = 1-(1 - lambda)^3
+# solve the above equation for lambda manually (eg Wolfram Alpha)
+# lambda = 0.049
 
 
 #will remove last age
@@ -19,15 +20,16 @@ max_age <- 61
 years <- 2010:2110
 N0 <- 1 # population size
 no_infection <- N0
-lambda <- rep(0.037, length=length(years))
+lambda <- rep(0.049, length=length(years))
 P_D <- c(0.005, 0.05, 0.00001, 0.00001)
+#probability of disease given inf for 3 serotype model (vector needs to be length 3)
+P_D <- P_D[1:3]
 
 P_D_mat <- t(replicate(max_age,P_D))
 
 # Initialise dataframe ----------------------------------------------------
 
 col_names <- list(
-               "S4",
                "S3",
                "S2",
                "S1",
@@ -35,11 +37,9 @@ col_names <- list(
                "I1",
                "I2",
                "I3",
-               "I4",
                "D1",
                "D2",
                "D3",
-               "D4",
                "total")
 
 array_names <- vector("list", 3)
@@ -53,11 +53,11 @@ dengue <- array(data = 0,
                 dimnames=array_names)
 
 
-dengue[1,,"S4"] <- N0 # add 100 to S4 to each age group in first year
-dengue[,1,"S4"] <- N0 # add 100 to S4 of 0 age group in all years (births)
+dengue[1,,"S3"] <- N0 # add 100 to S3 to each age group in first year
+dengue[,1,"S3"] <- N0 # add 100 to S3 of 0 age group in all years (births)
 
-dengue[1,,"total"] <- dengue[1,,"S4"] # add total to first year
-dengue[,1,"total"] <- dengue[,1,"S4"] # add total to first age
+dengue[1,,"total"] <- dengue[1,,"S3"] # add total to first year
+dengue[,1,"total"] <- dengue[,1,"S3"] # add total to first age
 
 # Populate dataframe ------------------------------------------------------
 
@@ -67,43 +67,36 @@ infection_probs <- function( lambda, max_age, P_D_mat, df, verbose=1 ){
   for (y in 1:(length(years)-1)) {
 
     # infection probabilities
-    P_I0_I1 <- 1-(1 - lambda[y])^4
-    P_I1_I2 <- 1-(1 - lambda[y])^3
-    P_I2_I3 <- 1-(1 - lambda[y])^2
-    P_I3_I4 <- 1-(1 - lambda[y])^1
+    P_I0_I1 <- 1-(1 - lambda[y])^3
+    P_I1_I2 <- 1-(1 - lambda[y])^2
+    P_I2_I3 <- 1-(1 - lambda[y])^1
+    P_I3_I4 <- 0L
     P_I4_I5 <- 0L
     
     # S4
-    df[y+1, 1:max_age + 1 , "S4"] <- df[y, 1:max_age, "S4"] * (1-P_I0_I1) # probability of individual staying susceptible
+    df[y+1, 1:max_age + 1 , "S3"] <- df[y, 1:max_age, "S3"] * (1-P_I0_I1) # probability of individual staying susceptible
     
     # S3
-    df[y+1, 1:max_age + 1, "S3"] <- df[y, 1:max_age, "S4"] * (P_I0_I1) + # prob of people with first infection 
-      (df[y, 1:max_age, "S3"] * (1-P_I1_I2) ) # probability of not getting a second infection
+    df[y+1, 1:max_age + 1, "S2"] <- df[y, 1:max_age, "S3"] * (P_I0_I1) + # prob of people with first infection 
+      (df[y, 1:max_age, "S2"] * (1-P_I1_I2) ) # probability of not getting a second infection
     
     # I1 (reminder: first infections occur during this year) - that's why there's no +1's
-    df[y, 1:max_age, "I1"] <- df[y,1:max_age, "S4"] * (P_I0_I1)  # Number of new first infections
+    df[y, 1:max_age, "I1"] <- df[y,1:max_age, "S3"] * (P_I0_I1)  # Number of new first infections
     
     # S2
-    df[y+1, 1:max_age+1, "S2"] <- df[y, 1:max_age, "S3"] * (P_I1_I2) + # prob of people with second infection 
-      df[y, 1:max_age, "S2"] * (1-P_I2_I3) # probability of not getting a third infection
+    df[y+1, 1:max_age+1, "S1"] <- df[y, 1:max_age, "S2"] * (P_I1_I2) + # prob of people with second infection 
+      df[y, 1:max_age, "S1"] * (1-P_I2_I3) # probability of not getting a third infection
     
     # I2
-    df[y, 1:max_age, "I2"] <- df[y, 1:max_age, "S3"] * (P_I1_I2)   # Number of new second infections
+    df[y, 1:max_age, "I2"] <- df[y, 1:max_age, "S2"] * (P_I1_I2)   # Number of new second infections
     
     # S1
-    df[y+1, 1:max_age+1, "S1"] <- df[y, 1:max_age, "S2"] * (P_I2_I3) + # prob of people with third infection 
-      (df[y, 1:max_age, "S1"] * (1-P_I3_I4) ) # probability of not getting a fourth infection
+    df[y+1, 1:max_age+1, "S0"] <- df[y, 1:max_age, "S1"] * (P_I2_I3) + # prob of people with third infection 
+      (df[y, 1:max_age, "S0"] * (1-P_I3_I4) ) # probability of not getting a fourth infection
     
     #I3
-    df[y, 1:max_age, "I3"] <- df[y, 1:max_age, "S2"] * (P_I2_I3) # number of new third infections
-    
-    # S0
-    df[y+1, 1:max_age+1, "S0"] <- df[y, 1:max_age, "S1"] * (P_I3_I4) + # prob of people with fourth infection 
-      (df[y, 1:max_age, "S0"] * (1-P_I4_I5) ) # probability of not getting a fifth infection
-    
-    # I4
-    df[y, 1:max_age, "I4"] <- df[y, 1:max_age, "S1"] * (P_I3_I4) # number of new fourth infections
-    
+    df[y, 1:max_age, "I3"] <- df[y, 1:max_age, "S1"] * (P_I2_I3) # number of new third infections
+
     #calculate the total population across susceptible statuses
     df[y+1,1:max_age+1,"total"] <- rowSums(df[y+1,1:max_age+1,which(col_names %like% "S")])
     
@@ -147,15 +140,15 @@ for(i in 1:length(lambda_vals)){
   
 }
 
-
-plot(x = I2_vec/I2_vec[38], y = lambda_vals, type = "l", 
+#if using 4-serotype model, use I2_vec[38] and lambda_vals[38]
+plot(x = I2_vec/I2_vec[50], y = lambda_vals, type = "l", 
      xlab = "Modelled cases proportional to Colombo" , 
      ylab = "Force of Infection")
 abline(v = 1.0)
-abline(h = lambda_vals[38])
+abline(h = lambda_vals[50])
 abline(v = 0.77)
-#value of lambda where I2_vec/I2_vec[38] is closest to 38
-abline(h = lambda_vals[15])
+#value of lambda where I2_vec/I2_vec[38] is closest to 0.77
+abline(h = lambda_vals[21])
 
 
 # plot proportion of people with second infection at equilibrium relative to prop of people
@@ -165,21 +158,21 @@ abline(h = lambda_vals[15])
 font.size <- 2
 lwd <- 4
 
-plot(x = I2_vec/I2_vec[38], y = lambda_vals, type = "l", lwd=lwd,
+plot(x = I2_vec/I2_vec[50], y = lambda_vals, type = "l", lwd=lwd,
      xlab="Secondary infections relative to Colombo", ylab="Force of infection", cex.axis=font.size, cex.lab=font.size)
 abline(v = 1.0, lwd=lwd, lty=2)
-abline(h = lambda_vals[38], lwd=lwd, lty=2)
+abline(h = lambda_vals[50], lwd=lwd, lty=2)
 abline(v = 0.77, lwd=lwd, lty=2)
-#value of lambda where I2_vec/I2_vec[38] is closest to 38
-abline(h = lambda_vals[15], lwd=lwd, lty=2)
+#value of lambda where I2_vec/I2_vec[38] is closest to 0.77
+abline(h = lambda_vals[21], lwd=lwd, lty=2)
 
 #* Calculating rho ---------------------------------------------------------
 
-rho <- (100/10000)/I2_vec[38]
+rho <- (100/10000)/I2_vec[50]
 
 # Calculating I1 and I2 in Gampaha ----------------------------------------
 
-dengue_g_df <- infection_probs(lambda = rep(lambda_vals[15], length(years)), 
+dengue_g_df <- infection_probs(lambda = rep(lambda_vals[21], length(years)), 
                                max_age = max_age, 
                                P_D_mat = P_D_mat, 
                                df = dengue,
