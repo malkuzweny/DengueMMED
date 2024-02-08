fb53c8bb45fe05564eec22785083e370209ee934
malkuzweny <manaralkuzweny@gmail.com>
2024-02-08 11:49:36 -0500

    fixed figs

--- main_infections.R
+++ main_infections.R
@@ -207,7 +207,7 @@ plot(prop.infection$minAge, prop.infection$prop.infection)
 plotDF <- dengue_df.Gamp
 par(mar = c(5.1, 5.1, 4.1, 2.1))
 
-font.size <- 2
+font.size <- 1.3
 lwd <- 4
 
 # par(mar = c(bottom, left, top, right)) , alue for mar is c(5.1, 4.1, 4.1, 2.1)  
@@ -216,7 +216,7 @@ lwd <- 4
   cols <- brewer.pal(4, "Set1")
   
   plot( x=1:(max_age), y = plotDF[100,,"I1" ], type = "l", col=cols[1], lwd=lwd,
-        ylim = c(0, 0.08), xlab="Age", ylab="% of population infected", cex.axis=font.size, cex.lab=font.size)
+        ylim = c(0, 0.06), xlab="Age", ylab="% of population infected", cex.axis=font.size, cex.lab=font.size)
   
   lines( x = (1:max_age), y = plotDF[100,,"I2" ], col=cols[2], lwd=lwd)
   lines( x = (1:max_age), y = plotDF[100,,"I3" ], col=cols[3], lwd=lwd)
@@ -225,8 +225,8 @@ lwd <- 4
   abline(v=16)
   rect(xleft = 4, xright = 16, ybottom = par("usr")[3], ytop = par("usr")[4],
        border = NA, col = adjustcolor("black", alpha = 0.3))
-  legend(x = c(25, 60), y = c(0.05, 0.08), legend=c("First infection", "Second infection", "Third infection", "Fourth infection"),
-         col=c(cols[1], cols[2], cols[3], cols[4]), lty=1, cex=font.size, bg="white", bty="n", lwd=lwd)
+  legend(x = c(20, 55), y = c(0.03, 0.06), legend=c("First infection", "Second infection", "Third infection"),
+         col=c(cols[1], cols[2], cols[3]), lty=1, cex=font.size, bg="white", bty="n", lwd=lwd)
   
 }
 
--- main_samplesizeCalc.R
+++ main_samplesizeCalc.R
@@ -12,56 +12,28 @@ lwd <- 3 #presentation used 4
 # Do power calculation ----------------------------------------------------
 # Pi_0 equals inf_c (proportion of population experiencing the outcome) or case_c_2yr
 
-cl.perarm.inf <- run.sscalc(z_a2=1.96, z_b=0.84, pi_0=0.091, treatment_effect = 0.3, k=0.15, nr.percluster = 100)
+cl.perarm.inf <- run.sscalc(z_a2=1.96, z_b=0.84, pi_0=0.094, treatment_effect = 0.3, k=0.15, nr.percluster = 100)
 cl.perarm.cases <- run.sscalc(z_a2=1.96, z_b=0.84, pi_0=0.015, treatment_effect = 0.3, k=0.15, nr.percluster = 20:200)
 
 effectsizes <- c(0.25,0.3,0.35)
-k_vec <- c(0.02,0.15,0.25)
-
-
-
-# Different age groups ----------------------------------------------------
-
-# * Infection (active surv) -----------------------------------------------
-
-cl.perarm.inf <- vector()
-
-for (ii in prop.infection$prop.infection) {
-  cl.perarm.inf.ii <- run.sscalc(z_a2=1.96, z_b=0.84, pi_0=ii, treatment_effect = 0.3, k=0.15, nr.percluster = 200)
-  cl.perarm.inf <- rbind(cl.perarm.inf, cl.perarm.inf.ii)
-}
-
-prop.infection$cl.perarm.inf <- cl.perarm.inf
-prop.infection$sampleSize.inf <- prop.infection$cl.perarm.inf * 200
-prop.infection$prop.population <- prop.infection$sampleSize.inf / prop.infection$population * 2 # *2 bc two arms
-names(prop.infection) <- c("prop.infection", "minAge", "population", "maxAge", "clusters.perarm", "people.perarm", "prop.population")
-
-
-plot(x=prop.infection$minAge, y=prop.infection$clusters.perarm, type="l", col="black", 
-     xlab="Minimum age", ylab="Number of clusters per arm",
-     main="Effect size: 0.3, k:0.15, nr.percluster:200")
-
-plot(x=prop.infection$minAge, y=prop.infection$people.perarm, type="l", col="black", 
-     xlab="Minimum age", ylab="Number of participants per arm",
-     main="Effect size: 0.3, k:0.15, nr.percluster:200")
-
-plot(x=prop.infection$minAge, y=prop.infection$prop.population, type="l", col="black", 
-     xlab="Minimum age", ylab="Proportion of total population included in study",
-     main="Effect size: 0.3, k:0.15, nr.percluster:200")
-
-
-# only people aged 20+ is about 4500 per arm -> 9000 in total. we had 4500 in report (with 4 serotypes, and fewer per cluster)
-
+k_vec <- c(0.2,0.15,0.25)
 
 # plots for presentations -------------------------------------------------
 # infection
 par(mar = c(5.1, 5.1, 4.1, 2.1))
 
+cl.perarm.inf_k2_e2 <- run.sscalc(z_a2=1.96, z_b=0.84, pi_0=0.094, 
+                                  treatment_effect = effectsizes[2], k = k_vec[2], 
+                                  nr.percluster = 20:200)
+cl.perarm.case_k2_e2 <- run.sscalc(z_a2=1.96, z_b=0.84, pi_0=0.015, 
+                                  treatment_effect = effectsizes[2], k = k_vec[2], 
+                                  nr.percluster = 20:2000)
+
 plot(x=20:200, y=cl.perarm.inf_k2_e2, type="l", col="black", ylim=c(0,20), lwd=lwd,
      xlab="Number of children per cluster", ylab="Number of clusters per arm",
      main="Effect size: 0.3", cex.axis=font.size, cex.lab=font.size, cex.main=font.size)
 abline(h=15, lwd=lwd, lty=2)
-abline(v=148, lwd=lwd, lty=2)
+abline(v=c(20:200)[124], lwd=lwd, lty=2)
 
 # disease
 plot(x=20:2000, y=cl.perarm.case_k2_e2, type="l", col="black", lwd=lwd,
@@ -69,7 +41,7 @@ plot(x=20:2000, y=cl.perarm.case_k2_e2, type="l", col="black", lwd=lwd,
      main="Effect size: 0.3", cex.axis=font.size, cex.lab=font.size, cex.main=font.size,
      log="y", ylim=c(10,500), xlim=c(0,1000))
 abline(h=15, lwd=lwd, lty=2)
-abline(v=980, lwd=lwd, lty=2)
+abline(v=c(20:2000)[948], lwd=lwd, lty=2)
 
 
 # plots for report --------------------------------------------------------
@@ -120,7 +92,7 @@ run.sscalc(z_a2=1.96, z_b=0.84, pi_0=0.091,
            treatment_effect = effectsizes[2], k = k_vec[2], 
            nr.percluster = 20:200) # 149 per arm
 
-## infection endpoint, k=0.02, effect size=0.3
+## infection endpoint, k=0.2, effect size=0.3
 run.sscalc(z_a2=1.96, z_b=0.84, pi_0=0.091, 
            treatment_effect = effectsizes[2], k = k_vec[1], 
            nr.percluster = 20:200) # 116 per arm (22% less)
@@ -136,7 +108,7 @@ run.sscalc(z_a2=1.96, z_b=0.84, pi_0=0.015,
            treatment_effect = effectsizes[2], k = k_vec[2], 
            nr.percluster = 20:2000) # 967 per arm
 
-## case endpoint, k=0.02, effect size=0.35
+## case endpoint, k=0.2, effect size=0.35
 run.sscalc(z_a2=1.96, z_b=0.84, pi_0=0.015, 
            treatment_effect = effectsizes[2], k = k_vec[1], 
            nr.percluster = 20:2000) # 753 per arm (22% less)
@@ -145,3 +117,4 @@ run.sscalc(z_a2=1.96, z_b=0.84, pi_0=0.015,
 run.sscalc(z_a2=1.96, z_b=0.84, pi_0=0.015, 
            treatment_effect = effectsizes[2], k = k_vec[3], 
            nr.percluster = 1000:3000) # 1994 per arm (106% more)
+
